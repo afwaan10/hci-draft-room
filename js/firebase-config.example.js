186892230019548783a31760607f37b1e52943f9
afwaan10 <muhammaddeha03@gmail.com>
2026-07-03 04:17:05 +0700

    Add files via upload

---
+++ js/firebase-config.example.js
@@ -0,0 +1,9 @@
+// Paste config Firebase kamu di sini.
+window.HCI_FIREBASE_CONFIG = {
+  apiKey: "PASTE_API_KEY_HERE",
+  authDomain: "PASTE_PROJECT.firebaseapp.com",
+  projectId: "PASTE_PROJECT_ID",
+  storageBucket: "PASTE_PROJECT.appspot.com",
+  messagingSenderId: "PASTE_SENDER_ID",
+  appId: "PASTE_APP_ID"
+};
